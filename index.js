e5046ee542db0b5be35b8459ce5d27dc624da8b5
rizky969 <79305089+rizky969@users.noreply.github.com>
2021-02-23 07:32:46 +0700

    index.js

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ const menu = require("./lib/menu.js")
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
 const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const instagram = 'rizky-gans- gamtenkk
+
+
+_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // Rizky Ganss
+            + 'ORG:VVIPINDO;\n' // BOT RIZKY GANSS
+
+            + 'TEL;type=CELL;type=VOICE;waid=6289678654813:+62 89678654813\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
@@ -1450,7 +1454,7 @@ Silakan pilih :
 
 
 }
-//Hay gay
-//create @agusalnsyh don't delate this please
+//Hay Kawan
+//create @RizkyGanss don't delate this please
 	
 })
